bb1c1ad1f95e4d605bcdd5f928bcd99bdf3f5239
nivedabhat <157083128+nivedabhat@users.noreply.github.com>
2025-06-18 18:54:03 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python pdf_spec_parser/spec_parser.py
-python "notebook/sythetic_data gereation_time_series_model.py"
+python "notebook/synthetic_data_gereation_time_series_model.py"
 python notebook/model.py
 source venv/bin/activate
 python notebook/rul_prod_data.py
