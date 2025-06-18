21608aa391e1986b6210626a138aa1bb07497e8a
nivedabhat <157083128+nivedabhat@users.noreply.github.com>
2025-06-18 17:54:03 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,4 @@
-/bin/bash
-# Script to set up environment and run all required commands sequentially
-
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python pdf_spec_parser/spec_parser.py
-python "notebook/sythetic_data gereation_time_series_model.py"
-python "notebook/sythetic_data gereation_time_series_model.py"
-python notebook/model.py
-source venv/bin/activate
-python notebook/rul_prod_data.py
-chmod +x start.sh
-bash start.sh
- #!/bin/bash
+#!/bin/bash
 # Script to set up environment and run all required commands sequentially
 
 # Initialize a Python virtual environment and install dependencies
